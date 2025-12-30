cd9bfc05d2642bcf7c184b7faa9bea2dd7d45821
MUKUL SARSWAT <137614804+mukulsarswat@users.noreply.github.com>
2025-12-30 15:46:55 +0530

    Add comments for code clarity in digital_clock.cpp
    Added comments to clarify the purpose of code lines.

--- DigitalClock/digital_clock.cpp
+++ DigitalClock/digital_clock.cpp
@@ -7,16 +7,16 @@ using namespace std;
 
 int main()  {
     while (true)    {
-        auto now = chrono::system_clock::now();
-        time_t currentTime = chrono::system_clock::to_time_t(now);
+        auto now = chrono::system_clock::now();   // New leaarning
+        time_t currentTime = chrono::system_clock::to_time_t(now);  // time instant
 
         tm* localTime = localtime(&currentTime);
 
         // system("cls"); for windows
 
-        system("clear");
+        system("clear");         // system call
 
-        cout<< put_time(localTime, "%H:%M:%S")<<endl;
+        cout<< put_time(localTime, "%H:%M:%S")<<endl;    // print time on the terminal
 
         this_thread::sleep_for(chrono::seconds(1));
     }
